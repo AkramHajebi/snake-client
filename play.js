cd649e32a2256e85a78a5b1a602cf2ecdf0b6990
AkramHajebi <ahajebi2000@email.com>
2020-12-01 23:59:14 +0000

    import client.js

--- play.js
+++ play.js
@@ -3,23 +3,7 @@ const net = require('net');
 /**
  * Establishes connection with the game server
  */
-const connect = function() {
-
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8');
-  conn.on('data', (data) => {  // conn is client
-    console.log(data);
-    //return data
-  });
-
-  //conn.write(action());
-
-  return conn;
-};
+const { connect } = require('./client');
 
 console.log('Connecting ...');
 connect();
