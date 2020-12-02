9ba0c9d91478ef46889e4e6111cc20a7d91e8a52
AkramHajebi <ahajebi2000@email.com>
2020-12-02 01:08:18 +0000

    asking client name

--- client.js
+++ client.js
@@ -7,12 +7,24 @@ const connect = function() {
     host: "localhost",
     port: 50541
   });
+
+  conn.on("data", (data) => console.log('haha'));
+
+
   // interpret incoming data as text
   conn.setEncoding('utf8');
   conn.on('data', (data) => {  // conn is client
     console.log(data);
+    console.log("Successfully connected to game server");
+    console.log('Name: Ah');
     //return data
-  });
+  }); 
+  
+  // conn.on('connection', () => {  // any string
+  // console.log('how are you?');
+    
+ //});
+
 
   //conn.write(action());
 
