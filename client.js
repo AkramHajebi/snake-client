4695d7e17ade01107808d46590ca1f98356cc66a
AkramHajebi <ahajebi2000@email.com>
2020-12-03 23:27:08 +0000

    write to move snake

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 const net = require('net');
+const { setTimeout } = require('timers');
 
 const connect = function() {
 
@@ -6,35 +7,47 @@ const connect = function() {
     host: 'localhost',
     port: 50541
   });
+  
+
 
   // assign a random name for client = conn
   let clientName = Math.random().toString(36).substring(3);
+  let movements = ["Move: up", "Move: down", "Move: left", "Move: right"];
 
   // interpret incoming data as text
   conn.setEncoding('utf8');
 
   // handeling data from server
-  conn.on('data', (data) => {  // conn is client
-    console.log(data);
+  // conn.on('data', (data) => {  // conn is client
+  //   console.log(data);
   
-  });
+  // });
 
-  conn.on('connect', () => {  // conn is client     
+  conn.on('connect', () => {  // conn is client  
+    
+    let time = 0;
+    // send server to do movements
+    for (const move of movements) {
+      
+      setTimeout( () => {
+        conn.write(move);
+        console.log(move);
+      }, time);  
+      
+      time += 50;
+      
+    }; 
+
     console.log("Successfully connected to game server");
+    conn.write(`Name: ${clientName}`);
+    
+
    }); 
 
-   conn.write(`Name: ${clientName}`);
-
+   
   return conn;
 
  
-
-  //  conn.on('connect',()=>{ 
-  //    setTimeout(function(){ conn.write("Move: up");}, 50);
-  //  });
-    
-    
-  //return conn;
 };
 
 module.exports = {connect};
