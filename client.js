43834ed526bde51351f2230f613ce458377ad25f
AkramHajebi <ahajebi2000@email.com>
2020-12-04 09:37:16 +0000

    setting up stdin and ctrl c keyboard control

--- client.js
+++ client.js
@@ -8,8 +8,6 @@ const connect = function() {
     port: 50541
   });
   
-
-
   // assign a random name for client = conn
   let clientName = Math.random().toString(36).substring(3);
   let movements = ["Move: up", "Move: down", "Move: left", "Move: right"];
@@ -17,31 +15,24 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding('utf8');
 
-  // handeling data from server
-  // conn.on('data', (data) => {  // conn is client
-  //   console.log(data);
-  
-  // });
-
-  conn.on('connect', () => {  // conn is client  
+conn.on('connect', () => {  // conn is client  
     
-    let time = 0;
-    // send server to do movements
-    for (const move of movements) {
+    // let time = 0;
+    // // send server to do movements
+    // for (const move of movements) {
       
-      setTimeout( () => {
-        conn.write(move);
-        console.log(move);
-      }, time);  
+    //   setTimeout( () => {
+    //     conn.write(move);
+    //     console.log(move);
+    //   }, time);  
       
-      time += 50;
+    //   time += 50;
       
-    }; 
+    // }; 
 
     console.log("Successfully connected to game server");
     conn.write(`Name: ${clientName}`);
     
-
    }); 
 
    
--- play.js
+++ play.js
@@ -1,9 +1,39 @@
 const net = require('net');
 
 /**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+   const stdin = process.stdin;
+   stdin.setRawMode(true);
+   stdin.setEncoding('utf8');
+   stdin.resume();
+
+   stdin.on('data', handleUserInput);
+
+   return stdin;
+};
+
+
+const handleUserInput = function(key) {
+  if(key==='\u0003') {
+    process.exit();
+  }
+ };
+
+
+
+//
+//const {setupInput} = require('./setupInterface');
+
+
+/**
  * Establishes connection with the game server
  */
 const { connect } = require('./client');
+
+
 
 console.log('Connecting ...');
 connect();
